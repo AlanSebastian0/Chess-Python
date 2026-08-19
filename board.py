d8ba2d6edefdd9b8175890d72cbe7cda9315ab6b
Alan Sebastian <alan8seb@gmail.com>
2026-08-19 13:38:07 +0930

    legal_move filter make to is_check to unmake to generate all legal moves, castling rights and enpassant logic should be fully implemented now, make_move siginficant additions to store past rights. Pawn promotion has also been aded. make_move and unmake_move aare completed (should be). Special moves done. No copying entire board only history stack. next is to have a 'perft' a performance testing mechanism which checks all of the possible moves upto a certain depth, the number of legal moves should match what is found globally from the starting position

--- board.py
+++ board.py
@@ -3,7 +3,8 @@ dangerous_offsets = [
             (1, 1), (1, -1), (-1, -1), (-1, 1)   # Diagonal (indices 4-7)
         ]
 knight_moves = [(-2,-1),(-2,1),(-1,-2),(-1,2),(1,-2),(1,2),(2,-1),(2,1)]
-class Board: 
+
+class Board:
     #Rook = -4, knight = -2, Queen = -5, King = -6, bishop = -3 pawn = -1. Negative numbers indicate black and positive are white
     def __init__(self):
         self.grid = self.start_grid()
@@ -45,16 +46,73 @@ class Board:
             return False
         return True
 
-    def make_move(self,start_square: tuple, end_square: tuple): #make move does not change the turns, MANUAL self.turn is required
+    def make_move(self,start_square: tuple, end_square: tuple, promotion_piece: int = None): #make move does not change the turns, MANUAL self.turn is required
         piece = self.get_piece(start_square)
         captured_piece = self.get_piece(end_square) #store captured piece before overwriting
-        if piece == 6: self.white_king_location = end_square #update the kings location when it moves to common variable
-        if piece == -6: self.black_king_location = end_square
         Srow,Scol = start_square
         Erow,Ecol = end_square
+
+        #save current state for unmake_move before they are changed
+        old_ep = self.en_passant_sq
+        old_rights = self.castling_rights.copy() #must copy dictionary so it doesnt reference the same memory 
+        is_en_passant = False
+        is_castle = False
+        #en passant logic 
+        if abs(piece) == 1 and Scol != Ecol and captured_piece == 0: #diagonal move to empty square means en passant
+            is_en_passant = True
+            captured_piece = self.grid[Srow][Ecol] #the actual piece captured is adjacent to start square
+            self.grid[Srow][Ecol] = 0 #remove the captured pawn from the board
+
+        #castling logic
+        if abs(piece) == 6 and abs(Scol - Ecol) == 2: #king moving two squares means castling K = 6
+            is_castle = True
+            #logic is true for both colours
+            if Ecol == 6: #kingside castle
+                self.grid[Erow][5] = self.grid[Erow][7] #move rook next to king
+                self.grid[Erow][7] = 0 #empty original rook square
+            elif Ecol == 2: #queenside castle
+                self.grid[Erow][3] = self.grid[Erow][0]
+                self.grid[Erow][0] = 0
+
+        #update king location trackers
+        if piece == 6: self.white_king_location = end_square #update the kings location when it moves to common variable
+        if piece == -6: self.black_king_location = end_square
+
+        #move the main piece on the grid
         self.grid[Srow][Scol] = 0
-        self.grid[Erow][Ecol] = piece
-        self.history.append((start_square, end_square, piece, captured_piece)) #store the move and captured piece for unmake_move
+
+        #promotion logic, this is also handled in move_generation.py
+        if promotion_piece is not None:
+            self.grid[Erow][Ecol] = promotion_piece
+        else:
+            self.grid[Erow][Ecol] = piece
+
+        if abs(piece) == 1 and abs(Srow - Erow) == 2: 
+        #set en passant square for NEXT move if pawn moved two squares
+            self.en_passant_sq = ((Srow + Erow) // 2, Scol) #the square directly behind the jumping pawn
+        else:
+            self.en_passant_sq = None
+
+        #update castling rights if king or rooks move, or if a rook is captured in its corner
+        self.update_castling_rights(start_square, end_square)
+
+        self.history.append((start_square, end_square, piece, captured_piece, old_ep, old_rights, is_en_passant, is_castle)) 
+        #store EVERYTHING needed to reverse this move
+
+    def update_castling_rights(self, start_square: tuple, end_square: tuple): #helper function to update castling_rights if a king or rook is moved castling rights are apprioately cancelled
+        #white rights
+        if start_square == (7,4): #white king moved
+            self.castling_rights["white_kingside"] = False
+            self.castling_rights["white_queenside"] = False
+        if start_square == (7,7) or end_square == (7,7): self.castling_rights["white_kingside"] = False #white kingside rook moved or captured
+        if start_square == (7,0) or end_square == (7,0): self.castling_rights["white_queenside"] = False #white queenside rook moved or captured
+
+        #black rights
+        if start_square == (0,4): #black king moved
+            self.castling_rights["black_kingside"] = False
+            self.castling_rights["black_queenside"] = False
+        if start_square == (0,7) or end_square == (0,7): self.castling_rights["black_kingside"] = False
+        if start_square == (0,0) or end_square == (0,0): self.castling_rights["black_queenside"] = False
 
     def is_check(self) -> bool:
         # Get king square for current turn
@@ -103,13 +161,38 @@ class Board:
     def unmake_move(self): #undoes the last made move using history stack
         if not self.history:
             return #nothing to undo
-        start_square, end_square, moved_piece, captured_piece = self.history.pop()
-        if moved_piece == 6: self.white_king_location = start_square #restore kings location to starting square
-        if moved_piece == -6: self.black_king_location = start_square
+
+        #pop all 8 variables saved during make_move, also removes them fro storage
+        start_square, end_square, moved_piece, captured_piece, old_ep, old_rights, is_en_passant, is_castle = self.history.pop()
         Srow, Scol = start_square
         Erow, Ecol = end_square
-        self.grid[Srow][Scol] = moved_piece #put moved piece back
-        self.grid[Erow][Ecol] = captured_piece #restore captured piece or 0
+
+        #restore global board states
+        self.en_passant_sq = old_ep
+        self.castling_rights = old_rights
+
+        #restore kings location to starting square
+        if moved_piece == 6: self.white_king_location = start_square 
+        if moved_piece == -6: self.black_king_location = start_square
+        
+        #3. put moved piece back
+        self.grid[Srow][Scol] = moved_piece 
+
+        #handle grid restoration based on move type
+        if is_en_passant:
+            self.grid[Erow][Ecol] = 0 #square pawn moved to is empty again
+            self.grid[Srow][Ecol] = captured_piece #put captured pawn back beside the starting square
+        else:
+            self.grid[Erow][Ecol] = captured_piece #restore captured piece or 0 on the destination square
+
+         #restore rook position if it was a castle
+        if is_castle:
+            if Ecol == 6: #kingside
+                self.grid[Erow][7] = self.grid[Erow][5] #put rook back in corner
+                self.grid[Erow][5] = 0 #empty the square next to king
+            elif Ecol == 2: #queenside
+                self.grid[Erow][0] = self.grid[Erow][3]
+                self.grid[Erow][3] = 0
 
     def __repr__(self):
         output = ""
--- move_generation.py
+++ move_generation.py
@@ -6,6 +6,7 @@ rook_offsets = [(0,1),(0,-1),(1,0),(-1,0)]
 bishop_offsets = [(1,1),(1,-1),(-1,-1),(-1,1)]
 king_offsets = rook_offsets + bishop_offsets
 queen_offsets = rook_offsets + bishop_offsets
+
 def pseudo_legal_moves(board: Board) -> list: #pseudo legal check filtering has not been added yet
     moves = [] #store a list of tuples of the piece starting square and its ending sqaure
     for row in range(8):
@@ -16,32 +17,56 @@ def pseudo_legal_moves(board: Board) -> list: #pseudo legal check filtering has 
                 continue
             if board.is_own_piece(piece):
                 piece_type = abs(piece)
+                
                 if piece_type == 1:
                     if piece > 0: #white pawn
                         square_ahead = (row-1,col) #coordinates for the square infront of the pawn
                         if board.in_bounds(square_ahead) and board.get_piece(square_ahead) == 0: #check if square is vacant
-                            moves.append((start_pos, square_ahead))
-                            square_ahead = (row-2, col)
-                            if row == 6 and board.get_piece(square_ahead) == 0: #double square checking, nested if here for ease
+                            if row - 1 == 0: #pawn promotion logic
+                                for promo in [5, 4, 3, 2]: #queen, rook, bishop, knight
+                                    moves.append((start_pos, square_ahead, promo))
+                            else:
                                 moves.append((start_pos, square_ahead))
+                                square_ahead = (row-2, col)
+                                if row == 6 and board.get_piece(square_ahead) == 0: #double square checking, nested if here for ease
+                                    moves.append((start_pos, square_ahead))
+                                    
                         for d_col in [1,-1]: #diagonal logic
                             diagonal_square = (row-1, col+d_col)
                             if board.in_bounds(diagonal_square):
                                 destination_piece = board.get_piece(diagonal_square) #store the 'piece' if it is in bounds
                                 if destination_piece != 0 and not board.is_own_piece(destination_piece): #if not empty and is not the same colour
+                                    if row - 1 == 0: #pawn promotion on capture
+                                        for promo in [5, 4, 3, 2]:
+                                            moves.append((start_pos, diagonal_square, promo))
+                                    else:
+                                        moves.append((start_pos, diagonal_square))
+                                elif diagonal_square == board.en_passant_sq: #en passant capture logic
                                     moves.append((start_pos, diagonal_square))
+                    
                     else: #black pawn
                         square_ahead = (row+1, col)
                         if board.in_bounds(square_ahead) and board.get_piece(square_ahead) == 0: #logic for vacant square
-                            moves.append((start_pos, square_ahead))
-                            square_ahead = (row+2, col)
-                            if row == 1 and board.get_piece(square_ahead) == 0:
+                            if row + 1 == 7: #pawn promotion logic
+                                for promo in [-5, -4, -3, -2]: #queen, rook, bishop, knight (negative for black)
+                                    moves.append((start_pos, square_ahead, promo))
+                            else:
                                 moves.append((start_pos, square_ahead))
+                                square_ahead = (row+2, col)
+                                if row == 1 and board.get_piece(square_ahead) == 0:
+                                    moves.append((start_pos, square_ahead))
+                                    
                         for d_col in [1,-1]:
                             diagonal_square = (row+1,col+d_col)
                             if board.in_bounds(diagonal_square):
                                 destination_piece = board.get_piece(diagonal_square)
                                 if destination_piece != 0 and not board.is_own_piece(destination_piece):
+                                    if row + 1 == 7: #pawn promotion on capture
+                                        for promo in [-5, -4, -3, -2]:
+                                            moves.append((start_pos, diagonal_square, promo))
+                                    else:
+                                        moves.append((start_pos, diagonal_square))
+                                elif diagonal_square == board.en_passant_sq: #en passant capture logic
                                     moves.append((start_pos, diagonal_square))
 
                 elif piece_type == 2: #knight logic
@@ -56,16 +81,30 @@ def pseudo_legal_moves(board: Board) -> list: #pseudo legal check filtering has 
                     rook_moves = generate_sliding_legal_moves(board,start_pos, rook_offsets)
                     moves += rook_moves
 
-                elif piece_type == 5: #queen
+                elif piece_type == 5: #queen logic
                     queen_moves = generate_sliding_legal_moves(board, start_pos, queen_offsets)
                     moves += queen_moves
 
-                elif piece_type == 6:
+                elif piece_type == 6: #king
                     king_moves = generate_singular_moves(board, start_pos,king_offsets)
                     moves += king_moves
                     
+                    #castling logic checks if rights exist and squares between king and rook are empty
+                    if piece > 0: #white castling
+                        if board.castling_rights["white_kingside"] and board.get_piece((7,5)) == 0 and board.get_piece((7,6)) == 0:
+                            #checks if the two squares are empy
+                            moves.append((start_pos, (7,6))) #checks 
+                        if board.castling_rights["white_queenside"] and board.get_piece((7,1)) == 0 and board.get_piece((7,2)) == 0 and board.get_piece((7,3)) == 0:
+                            #checks if the three squares are empty for the queen side castle
+                            moves.append((start_pos, (7,2)))
+                    else: #black castling
+                        if board.castling_rights["black_kingside"] and board.get_piece((0,5)) == 0 and board.get_piece((0,6)) == 0:
+                            moves.append((start_pos, (0,6)))
+                        if board.castling_rights["black_queenside"] and board.get_piece((0,1)) == 0 and board.get_piece((0,2)) == 0 and board.get_piece((0,3)) == 0:
+                            moves.append((start_pos, (0,2)))
+
     return moves
-            
+
 def generate_sliding_legal_moves(board: Board,start_pos: tuple, offset: list) -> list:
     #what to achieve apply offsets until object is hit
     moves = []
@@ -85,6 +124,7 @@ def generate_sliding_legal_moves(board: Board,start_pos: tuple, offset: list) ->
             e_row += d_row
             e_col += d_col
     return moves
+
 def generate_singular_moves(board: Board, start_pos: tuple, offset: list) -> list:
     moves = []
     row, col = start_pos #get the pieces row and column
@@ -95,10 +135,10 @@ def generate_singular_moves(board: Board, start_pos: tuple, offset: list) -> lis
         end_pos = (e_row, e_col) #store the end coordinates
         if board.in_bounds(end_pos): #continue if it is within bounds
             destination_piece = board.get_piece(end_pos)
-            if destination_piece != 0: 
+            if destination_piece != 0:
                 if not board.is_own_piece(destination_piece):
                     moves.append((start_pos, end_pos)) #if end coordinates is not empty but the occupied square is held by the opposite square it can be captured
-                continue 
+                continue
             moves.append((start_pos, end_pos)) #append move if it was empty
             continue
     return moves
@@ -106,11 +146,10 @@ def legal_moves(board: Board) -> list:
     moves = []
     pseudo_moves = pseudo_legal_moves(board)
     for move in pseudo_moves:
-        board.make_move(move[0], move[1])
+        board.make_move(*move) #the asterisk unpacks the tuple into 2 or 3 arguments dynamically (passes in automatically)
         if board.is_check(): #check if the pseudo move was illegal
             board.unmake_move()
-            continue 
+            continue
         board.unmake_move()
         moves.append(move) #append the moves if it is legal
     return moves
-
